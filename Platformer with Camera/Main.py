0afad0c98617d136427c3b3d017f95ea137d3565
TafaSakho <tafa.sakho@edu.esiee.fr>
2023-09-20 09:55:34 +0200

    Vies du player

--- Platformer with Camera/Main.py
+++ Platformer with Camera/Main.py
@@ -1,3 +1,7 @@
+from src.cam import *
+from src.maploader import MapLoader
+from src.block import Block
+from src.player import Player
 import pygame
 
 from pygame.locals import *
@@ -6,12 +10,9 @@ import random
 
 sys.path.append('src')
 
-from src.player import Player
-from src.block import Block
-from src.maploader import MapLoader
-from src.cam import *
 
 pygame.init()
+
 
 class Game():
     def __init__(self):
@@ -23,7 +24,8 @@ class Game():
         self.screen_res = [750, 500]
 
         self.font = pygame.font.SysFont("Impact", 55)
-        self.screen = pygame.display.set_mode(self.screen_res, pygame.HWSURFACE, 32)
+        self.screen = pygame.display.set_mode(
+            self.screen_res, pygame.HWSURFACE, 32)
 
         self.entities = pygame.sprite.Group()
         self.solids = pygame.sprite.Group()
@@ -31,22 +33,21 @@ class Game():
         self.maploader.load(1)
         self.player = self.maploader.player
         self.camera = self.maploader.camera
-        
+
         self.entities.add(self.solids)
         self.entities.add(self.player)
 
         self.clock.tick(60)
         while 1:
             self.Loop()
-            
+
     def Loop(self):
         # main game loop
         self.eventLoop()
-        
+
         self.Tick()
         self.Draw()
         pygame.display.update()
-
 
     def eventLoop(self):
         # the main event loop, detects keypresses
@@ -58,7 +59,7 @@ class Game():
                 if event.key == K_RETURN:
                     self.reset()
                     self.maploader.load(2)
-                    
+
                 if event.key == K_SPACE:
                     self.player.jump()
 
@@ -77,16 +78,22 @@ class Game():
         self.entities.add(self.player)
 
     def Draw(self):
-        self.screen.fill((150,150,150))
+        self.screen.fill((150, 150, 150))
 
-        for l in self.maploader.layers[::-1]:#Update layers (have to reverse list to blit properly)
-            self.screen.blit(l.image, self.camera.apply_layer(l))                
+        # Update layers (have to reverse list to blit properly)
+        for l in self.maploader.layers[::-1]:
+            self.screen.blit(l.image, self.camera.apply_layer(l))
 
         self.player.update(self.ttime / 1000.)
         self.camera.update(self.player)
-        
-        for e in self.entities: #update blocks etc.
+
+        for e in self.entities:  # update blocks etc.
             self.screen.blit(e.image, self.camera.apply(e))
+
+        # Draw the score to the screen
+        score_text = self.font.render(
+            f'Lives : {self.player.lives}', True, (255, 255, 255))
+        self.screen.blit(score_text, (10, 10))
 
 
 Game()
--- Platformer with Camera/src/player.py
+++ Platformer with Camera/src/player.py
@@ -11,9 +11,9 @@ class Player(pygame.sprite.Sprite):
         self.game = game
         pygame.sprite.Sprite.__init__(self)
 
-        self.image = pygame.Surface([25,50])
-        self.image.fill((0,0,255))
-        self.rect = self.image.get_rect(topleft=(pos[0],pos[1]))
+        self.image = pygame.Surface([25, 50])
+        self.image.fill((0, 0, 255))
+        self.rect = self.image.get_rect(topleft=(pos[0], pos[1]))
         self.true_location = list(self.rect.topleft)
         self.dx = 0
         self.dy = 0
@@ -25,28 +25,29 @@ class Player(pygame.sprite.Sprite):
         self.moving = False
         self.fall = False
 
-    #Credit for most Collision Detection goes to Mekire
+        self.lives = 3
 
-    def get_position(self,obstacles):
+    # Credit for most Collision Detection goes to Mekire
+
+    def get_position(self, obstacles):
         """Calculate where our player will end up this frame including
         collisions."""
         if not self.fall:
             self.check_falling(obstacles)
         else:
-            self.fall = self.check_collisions((0,self.dy),1,obstacles)
+            self.fall = self.check_collisions((0, self.dy), 1, obstacles)
         if self.dx:
-            self.check_collisions((self.dx,0),0,obstacles)
+            self.check_collisions((self.dx, 0), 0, obstacles)
 
-    def check_falling(self,obstacles):
+    def check_falling(self, obstacles):
         """Checks one pixel below the player to see if the player is still on
         the ground."""
-        test_rect = self.rect.move((0,1))
+        test_rect = self.rect.move((0, 1))
         obs_list = [obs.rect for obs in obstacles]
         if test_rect.collidelist(obs_list) == -1:
             self.fall = True
 
-
-    def check_collisions(self,offset,index,obstacles):
+    def check_collisions(self, offset, index, obstacles):
         """This function checks if a collision would occur after moving offset
         pixels.  If a collision is detected position is decremented by one pixel
         and retested.  This continues until we find exactly how far we can
@@ -55,11 +56,11 @@ class Player(pygame.sprite.Sprite):
 
         self.true_location[index] += offset[index]
         self.rect[index] = self.true_location[index]
-        
+
         while pygame.sprite.spritecollideany(self, obstacles):
-            self.rect[index] += (1 if offset[index]<0 else -1)
+            self.rect[index] += (1 if offset[index] < 0 else -1)
             unaltered = False
-            self.true_location[index] = self.rect[index] 
+            self.true_location[index] = self.rect[index]
         return unaltered
 
     def jump(self):
@@ -69,24 +70,20 @@ class Player(pygame.sprite.Sprite):
             self.fall = True
 
     def update(self, dt):
-        mx , my = pygame.mouse.get_pos()
+        mx, my = pygame.mouse.get_pos()
         keys = self.game.keys_pressed
         self.dx = 0
         self.dy = self.jump_power*dt
-        #Update keypresses
+        # Update keypresses
         if 1 in keys:
             if keys[K_d]:
-                 self.dx += self.speed*dt
+                self.dx += self.speed*dt
             if keys[K_q]:
-                 self.dx -= self.speed*dt
-        #Collision, get where player should be              
+                self.dx -= self.speed*dt
+        # Collision, get where player should be
         self.get_position(self.game.solids)
-        #Jumping
+        # Jumping
         if self.fall:
             self.jump_power += GRAV*dt
         else:
             self.jump_power = 0
-
-         
-
-            
